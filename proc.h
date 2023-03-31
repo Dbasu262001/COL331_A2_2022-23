ab6306d35b1e50f6f048b73c1904f245a57f85af
dbasu262001 <dharmes262000@gmail.com>
2023-03-31 19:37:16 +0530

    Added attribute to proc.h

--- proc.h
+++ proc.h
@@ -49,6 +49,11 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  
+  
+  //Added attributes  
+  int p_exec_time;
+  int p_deadline;
 };
 
 // Process memory is laid out contiguously, low addresses first:
